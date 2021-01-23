23ecf1e408cd39f363840bf24edb29c0e1d97516
Gustav Lau <gustavlau@gmail.com>
2021-01-23 00:11:32 -0500

    fixing image address link

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function fillNavButtons() {
     let navButtonsPics = document.querySelector(".nav-buttons").children;
     for(i=0; i<navButtonsPics.length; i++){
-        navButtonsPics[i].style.backgroundImage = `url(./img/FLex_Images/${i}.jpeg)`;
+        navButtonsPics[i].style.backgroundImage = `url(./img/header_img/${i}.jpeg)`;
         navButtonsPics[i].style.backgroundSize ="cover";
         console.log(navButtonsPics);
     }
