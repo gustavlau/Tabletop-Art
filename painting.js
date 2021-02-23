7c2e70c87c370c60349fb6398fdf94b864b56e35
Gustav Lau <gustavlau@gmail.com>
2021-02-23 00:14:59 -0500

    styled painting page some more

--- painting.js
+++ painting.js
@@ -4,9 +4,10 @@ const pickr = new Pickr({
     theme: 'nano', // or 'monolith', or 'nano'
     showAlways: true,
     useAsButton: true,
-    position: 'right-middle',
+    position: 'top',
     autoReposition: false,
     inline: true,
+    default: '#0066B3',
 
     swatches: [
         '#0066B3',
@@ -60,38 +61,58 @@ pickr.on('change', (color, instance) => {
 //Selects svg element and fills it with the colour picker's colour
 function colourTarget (e){
     var clickedItem = e.target
-    if(!clickedItem.classList.contains("outline") && !clickedItem.hasAttribute("viewBox")){
+    if(!clickedItem.classList.contains("outline") && !clickedItem.hasAttribute("viewBox") && !clickedItem.classList.contains("colouring-area")){ //prevents colouring the contour when clicking the white space near svg
         clickedItem.style.fill=colourStorage;
     }
 }
+
 document.addEventListener("click", colourTarget, false);
 
 //changes the svg based on clicked img
 function changeSvg (e){
     let svgTarget = e.target
     if(svgTarget.classList.contains("target-prot")){ // only works with the image gallery class
-        console.log(svgTarget.getAttribute("src"));
-        loadSvg("#svg",svgTarget.getAttribute("src"));
+        // console.log(svgTarget.getAttribute("src"));
+        loadSvg("#svg",svgTarget.getAttribute("src"));                
     }
 
 }
 
+
+loadSvg("#svg","/img/svg/svg1.svg");
 document.addEventListener("click", changeSvg, false);
 
 //loads in the svg file for inline HTML
 function loadSvg(selector, url) {
     let target = document.querySelector(selector);
     // Request the SVG file
-      let ajax = new XMLHttpRequest();
-      ajax.open("GET", url, true);
-      ajax.send();
-  
-      // Append the SVG to the target
-      ajax.onload = function() {
+    let ajax = new XMLHttpRequest();
+    ajax.open("GET", url, true);
+    ajax.send();
+
+    // Append the SVG to the target
+    ajax.onload = function() {
         target.innerHTML = ajax.responseText;
         target.classList.add("outline");
         document.querySelector("svg").setAttribute("height","100%");
-      }
-  }
+    }
+}
 
-let svgGalleryLoad = [];
+
+let clearColourBtn = document.querySelector(".btn-zoom-clear");
+
+clearColourBtn.addEventListener("click", clearColours);
+
+// function clearColours (){
+//     let svgParent = document.querySelector(".scaling-svg-container");
+//     let selectAllDescendantsOfSvg = svgParent.getElementsByTagName("*");
+//     let attributeSet = selectAllDescendantsOfSvg[3];
+//     if(attributeSet.firstChild.hasAttribute="path"){
+//         attributeSet.style.fill="#000000";
+//         console.log(selectAllDescendantsOfSvg[3]);   
+//     }
+// }
+
+function clearColours(){
+    
+}
